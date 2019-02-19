3d31e87deee5277624fc7a317f7b3498450cb9c7
Chris Mayes <coachchristac@gmail.com>
2019-02-19 02:21:48 -0500

    Fixed error in previous commit where tests not included.

---
+++ test/test_persistent-bugger.js
@@ -0,0 +1,11 @@
+const persistent = require('../codewars/persistent-bugger');
+const expect = require('chai').expect;
+
+describe('When the solutions are tried for the persistent challenge', function(){
+    it('solution one passes', function(){
+        expect(persistent.persistence(39)).to.equal(3);
+        expect(persistent.persistence(999)).to.equal(4);
+        expect(persistent.persistence(4)).to.equal(0);
+        expect(persistent.persistence(912312302)).to.equal(1);
+    })
+})
