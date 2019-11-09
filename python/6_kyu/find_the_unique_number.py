4ed3a5b2b188f4a9192cd660a1cb735d441667ce
Chris Mayes <coachchristac@gmail.com>
2019-11-08 21:28:20 -0500

    minor edits for readability w/ python

--- python/6_kyu/find_the_unique_number.py
+++ python/6_kyu/find_the_unique_number.py
@@ -1,8 +1,8 @@
 def find_uniq(arr):
-    if not( arr[0] is arr[1] ):
+    if arr[0] is not arr[1]:
         return arr[0] if arr[1] is arr[2] else arr[1]
     i = 1
     while i < len(arr):
-        if not(arr[i] is arr[0]):
+        if arr[i] is not arr[0]:
             return arr[i]
         i+=1
