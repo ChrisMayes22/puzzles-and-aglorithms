9a38408459083cae5a32233084ad81cd0ad42f42
Chris Mayes <coachchristac@gmail.com>
2020-01-13 18:12:56 -0500

    refactored find_anagrams solution

--- codewars/python/5_kyu/find_anagrams.py
+++ codewars/python/5_kyu/find_anagrams.py
@@ -1,4 +1,4 @@
-def anagrams(word, words):
+def anagrams(word, words):  ## First attempt
     arr = []
     test_dict = { "".join(sorted(word)): True }
     for w in words:
@@ -7,3 +7,8 @@ def anagrams(word, words):
     return arr
 
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
+
+def anagrams_2(word, words): ## Second Attempt
+    return([item for item in words if "".join(sorted(item)) == "".join(sorted(word))])
+
+print(anagrams_2('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
